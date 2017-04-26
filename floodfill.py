3422d15919eaab68fed0e022c25e857b2753b2f6
mitchmcdee <mitchmcdee@gmail.com>
2017-04-26 18:12:06 +1000

    Fixed tabs -> spaces

--- floodfill.py
+++ floodfill.py
@@ -1,18 +1,18 @@
 # init sizes
-width = 1000
-height = 1000
+width = 100
+height = 40
 
 # init empty grid
 g = [['0']*width for i in range(height)]
 
 # create diagonal border from top right down
 for i in range(len(g)):
-	g[i][width-i-1] = '1'
+    g[i][width-i-1] = '1'
 
 # print out grid before
 print('Before:')
 for i in range(len(g)):
-	print(''.join(g[i]))
+    print(''.join(g[i]))
 
 # add some spacing
 print('\n\n')
@@ -27,24 +27,24 @@ directions = [(1,0), (0,1), (-1,0), (0,-1)]
 
 # perform dfs
 while len(stack) != 0:
-	# get next node
-	node = stack.pop()
+    # get next node
+    node = stack.pop()
 
-	# check neighbours
-	for direction in directions:
-		x = node[0] + direction[0]
-		y = node[1] + direction[1]
-		neighbour = (x,y)
+    # check neighbours
+    for direction in directions:
+        x = node[0] + direction[0]
+        y = node[1] + direction[1]
+        neighbour = (x,y)
 
-		# check valid neighbour
-		if neighbour not in visited and x >= 0 and y >= 0 and x < width and y < height and g[y][x] == '0':
-			visited.add(neighbour)
-			stack.append(neighbour)
+        # check valid neighbour
+        if neighbour not in visited and x >= 0 and y >= 0 and x < width and y < height and g[y][x] == '0':
+            visited.add(neighbour)
+            stack.append(neighbour)
 
-	# fill node
-	g[node[1]][node[0]] = '1'
+    # fill node
+    g[node[1]][node[0]] = '1'
 
 # print out grid after
 print('After:')
 for i in range(len(g)):
-	print(''.join(g[i]))
+    print(''.join(g[i]))
