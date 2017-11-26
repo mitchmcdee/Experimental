211d601977f4565b3e995029a706a348ade97575
mitchmcdee <mitchmcdee@gmail.com>
2017-11-26 13:49:24 +1000

    cleanup

--- allSubsets.py
+++ allSubsets.py
@@ -1,8 +1,8 @@
-def permTwo(a):
+def permutation(a):
     bits = [0] * len(a)
     permTwoHelper(a, bits, 0)
 
-def permTwoHelper(a, bits, i):
+def permutationHelper(a, bits, i):
     if i == len(a):
         print([a[i] for i,b in enumerate(bits) if b])
         return
@@ -12,5 +12,5 @@ def permTwoHelper(a, bits, i):
 
 lower = 1
 upper = 3
-array = [i for i in range(lower, upper + 1)]
-permTwo(array)
+startSet = set([i for i in range(lower, upper + 1)])
+permutation(startSet)
