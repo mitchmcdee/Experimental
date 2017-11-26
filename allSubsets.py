2ca452abd299af47727e7a6188f1d846df6479f4
mitchmcdee <mitchmcdee@gmail.com>
2017-11-26 14:06:57 +1000

    okay all fixed

--- allSubsets.py
+++ allSubsets.py
@@ -1,16 +1,18 @@
-def permutation(a):
+def allSubsets(a):
     bits = [0] * len(a)
-    permutationHelper(a, bits, 0)
+    allSubsetsHelper(a, bits, 0)
 
-def permutationHelper(a, bits, i):
+def allSubsetsHelper(a, bits, i):
     if i == len(a):
         print([a[i] for i,b in enumerate(bits) if b])
         return
 
-    permutationHelper(a, [1 if index == i else v for index,v in enumerate(bits)], i + 1)
-    permutationHelper(a, [0 if index == i else v for index,v in enumerate(bits)], i + 1)
+    bits[i] = 0
+    allSubsetsHelper(a, bits, i + 1)
+    bits[i] = 1
+    allSubsetsHelper(a, bits, i + 1)
 
 lower = 1
 upper = 3
 startSet = [i for i in range(lower, upper + 1)]
-permutation(startSet)
+allSubsets(startSet)
